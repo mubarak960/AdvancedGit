6f49a568c4c882da7a97f670dfc68bb380a65713
mubarak.shaik <mbshaik960@gmail.com>
2024-02-14 03:33:44 +0530

    any

--- src/basic/Origination.java
+++ src/basic/Origination.java
@@ -8,6 +8,7 @@ public class Origination {
 		System.out.println("Loan is not heloc");
 		System.out.println("Loan is refinance");
 		System.out.println("Loan can be Construction");
+		System.out.println("Loan can't be anything else");
 	}
 
 }
