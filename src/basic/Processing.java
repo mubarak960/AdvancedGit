c7adec8ec698eced2a9cdbe37277acef3034aff5
mubarak960 <97887817+mubarak960@users.noreply.github.com>
2024-02-14 11:51:14 +0530

    Loan will be moved to underwriting

--- src/basic/Processing.java
+++ src/basic/Processing.java
@@ -5,6 +5,7 @@ public class Processing {
 	public static void main(String[] args) 
 	{
 		System.out.println("Loan is in processing");
+		System.out.println("Loan will be moved to underwriting");
 	}
 
 }
