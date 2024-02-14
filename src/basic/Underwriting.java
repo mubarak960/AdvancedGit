a76ce6718bcb35614ceb7ec7ff82da89f84657f8
mubarak960 <97887817+mubarak960@users.noreply.github.com>
2024-02-14 11:45:41 +0530

    Loan will move to closing has been added

--- src/basic/Underwriting.java
+++ src/basic/Underwriting.java
@@ -5,6 +5,7 @@ public class Underwriting {
 	public static void main(String[] args) 
 	{
 		System.out.println("Loan is in underwriting");
+		System.out.println("Loan will move to closing");
 	}
 
 }
